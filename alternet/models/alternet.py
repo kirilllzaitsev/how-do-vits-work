5a7edb6fec57ce0d8d99c2558b74506dba501c5c
kirilllzaitsev <kzaitsev88@gmail.com>
2023-08-10 22:23:05 +0200

    add plot_attention

--- alternet/models/alternet.py
+++ alternet/models/alternet.py
@@ -151,9 +151,11 @@ class AttentionBlockB(nn.Module):
         k=1,
         norm=nn.BatchNorm2d,
         activation=nn.GELU,
+        return_attn=False,
         **block_kwargs
     ):
         super().__init__()
+        self.return_attn = return_attn
         dim_out = dim_in if dim_out is None else dim_out
         attn = partial(LocalAttention, window_size=window_size, k=k)
         width = dim_in // self.expansion
@@ -190,10 +192,13 @@ class AttentionBlockB(nn.Module):
 
         x = self.conv(x)
         x = self.norm2(x)
+
         x, attn = self.attn(x)
 
         x = self.sd(x) + skip
 
+        if self.return_attn:
+            return x, attn
         return x
 
 
@@ -398,6 +403,37 @@ def dnn_18(num_classes=1000, stem=True, name="alternet_18", **block_kwargs):
         name=name,
         **block_kwargs
     )
+
+
+def plot_attention(att_mat, img_size):
+    # att_mat = torch.stack(att_mat).squeeze(1)
+
+    # Average the attention weights across all heads.
+    att_mat = torch.mean(att_mat, dim=1)
+
+    # To account for residual connections, we add an identity matrix to the
+    # attention matrix and re-normalize the weights.
+    residual_att = torch.eye(att_mat.size(1))
+    aug_att_mat = att_mat + residual_att
+    aug_att_mat = aug_att_mat / aug_att_mat.sum(dim=-1).unsqueeze(-1)
+
+    # Recursively multiply the weight matrices
+    joint_attentions = torch.zeros(aug_att_mat.size())
+    joint_attentions[0] = aug_att_mat[0]
+
+    for n in range(1, aug_att_mat.size(0)):
+        joint_attentions[n] = torch.matmul(aug_att_mat[n], joint_attentions[n - 1])
+
+    # Attention from the output token to the input space.
+    v = joint_attentions[-1]
+    grid_size = int(np.sqrt(aug_att_mat.size(-1)))
+    mask = v[0, 0:].reshape(grid_size, grid_size).detach().numpy()
+    # mask = v[0, 1:].reshape(grid_size, grid_size).detach().numpy()
+    import cv2
+
+    print(mask.shape)
+    mask = cv2.resize(mask / mask.max(), img_size)[..., np.newaxis]
+    return mask
 
 
 if __name__ == "__main__":
